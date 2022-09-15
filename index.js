0be2e77d39249fff49bdb3c08ce777e5f7b8c98e
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-09-15 16:02:18 -0400

    commit tree 28d1219b2783012af8eb1347014bbea3b46b8903

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 1 
+let num2 = 62
+
+let multiply = num1 * num2
+
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+
+let random = Math.floor(Math.random()) + 1
+
+
+let max = (Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 20))
